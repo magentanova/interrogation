de04915e89f21897270ff402b37580547b941e82
Justin Richards <justinrichards@justins-air.attlocal.net>
2015-10-25 18:46:38 -0500

    added interaction with test script

--- interrogate.js
+++ interrogate.js
@@ -1,144 +1,27 @@
-// JavaScript Document
+window.onload = function(){
 
-function addLine(div,ul,text,ellipsis){
-  li = document.createElement("LI"); 
-  li.className = "chatLine";
-  ul.appendChild(li);
-  li.scrollIntoView();
-  if(ul.offsetHeight >= div.offsetHeight && flag !== true){
-      ul.style.height = div.offsetHeight + "px";
-      flag = true;
-     }
-  if (ellipsis){
-    li.innerHTML = "...";
-    setTimeout(function(){setLiText(li,text)},750);
-  }
-  else{
-    li.innerHTML = "DETECTIVE: " + text;
-  }
+	// GLOBAL VARS
+	ENTER_CODE = 13
+	LEVELS = {
+		artie: localStorage.artieLevel ? localStorage.artieLevel : 0,
+		judd: localStorage.juddLevel ? localStorage.juddLevel : 0
+	}	
+
+	// UTILITY FUNCTIONS
+
+	// EVENT HANDLERS
+	var chatSubmissionHandler = function(e){
+		if (e.keyCode === ENTER_CODE) {
+			var charName = e.target.id,
+			response = theScript[charName][LEVELS[charName]].rotationLines[0],
+			responseEl = document.createElement('li')
+		responseEl.innerHTML = response
+		console.log(charName)
+		$('ul#' + charName).append(responseEl)
+		console.log(response)
+		window.responseEl = responseEl
+		}
+	}
+
+	$('input').keypress(chatSubmissionHandler)
 }
-
-function getHint(){
-  if ($("#hintPrompt").css("display") != "none"){
-    $("#hintPrompt").fadeToggle();
-    $("#hintContent").fadeToggle();
-  }
-}
-
-function initChat(){
-  cookieDict = $.cookie();
-  if (isEmpty(cookieDict)){
-    $.cookie("artie-level","1");
-    $.cookie("judd-level","1");
-  }
-
-  // $.ajax({
-  //  url: "init.php",
-  //  type: "post",
-  //  success: function(){
-  //   console.log('chat data initiated...')
-  //  },
-  //  error: function(jqXHR, textStatus, errorThrown){
-  //     alert(errorThrown);
-  //     setTimeout('updateChat()', 3000);
-  // }
-  // })
-}
-
-function isEmpty(ob){
-   for(var i in ob){ return false;}
-  return true;
-}
-
-
-function setLiText(li,text){
-  li.innerHTML = text;
-}
-
-function setScroll(){
-   var box = $(".prisonerResponse")[0]; //selector returns whole set. just get the first one.
-   box.scrollTop = box.scrollHeight;
-}
-
-function updatePrompt(promptText){
-  $("#hintContent").css("display","none");
-  $("#hintPrompt").css("display","block");
-  $("#hintPrompt").html('Click here for a new hint.');
-  $('#hintContent').html(promptText);
-}
-
-function updateChat(obj){
-   var divs = $(".prisonerResponse"),
-      uls = $(".responseList"),
-      inputs = $(".userInput");
-      flag = false;
-      prisoner = obj.id;
-
-  // identify the correct prisoner box
-   if (obj.id == "artie"){
-      i = 0;
-   }
-   else if (obj.id == "judd"){
-      i = 1;
-   }
-   
-   var div = divs[i],
-      ul = uls[i],
-      input = inputs[i],
-      li;
-
-   obj.onkeypress = function(e){
-      var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
-      if (charCode == 13) {
-        // read the openness level from the html data
-         // var level = div.getAttribute('data-level');
-         var key = prisoner + '-level';
-         var level = $.cookie(key);
-         console.log($.cookie());
-         console.log(level);
-         // read the input text
-         inputText = input.value;
-         // do ajax
-         $.ajax({
-           url: "prisonerResponse",
-           type: "post",
-           contentType:"application/json",
-           dataType:"json",
-           data: JSON.stringify({'prisoner':prisoner,'q':inputText,'level':level}),
-           success: function(response){
-              console.log(response);
-              // var promptText = $(xml).find('prompt').text();
-              // if (promptText){
-              //   updatePrompt(promptText,inputText);
-              // }
-
-              // // add the player's own comment to the narrative stream
-              // addLine(div,ul,inputText,false);
-              // // // add the response to the narrative stream
-              // setTimeout(function(){addLine(div,ul,responseText,true)},750);
-              // // // if we've advanced a level, darken the red border
-              // var newLevel = $(xml).find('level').text();
-              // if (parseInt(newLevel,10) > parseInt(level,10)){
-              //   console.log(newLevel + ' ' + level);
-              //   border = $(div).css("border-color");
-              //   match = border.match(/(\d\.*\d*)\)/);
-              //   opacity = parseFloat(match[1]);
-              //   console.log(opacity);
-              //   opacity += .1;
-              //   console.log(opacity);
-              //   $(div).css("border-color","rgba(255, 0, 0, " + opacity + ")");
-              //   console.log('css changed');
-              //   $(div).attr("data-level",newLevel);
-              // }
-
-           },
-           error: function(jqXHR, textStatus, errorThrown){
-              alert(errorThrown);
-              setTimeout('updateChat()', 3000);
-           }
-         })
-      // return ($(xml).find('response').text());
-      input.value = "";
-      }     
-   }
-} 
---
+++ script.js
@@ -0,0 +1,19 @@
+window.theScript = 
+	{
+		artie: {
+			0: {
+				rotationLines: [
+					"hi i'm wimpy artie"
+				],
+				advancementLine: ''
+			}
+		},
+		judd: {
+			0: {
+				rotationLines: [
+					"hi i'm cool judd"
+				],
+				advancementLine: ''
+			}
+		}
+	}
