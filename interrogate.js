e8e42c02307ed075409e759a8a8b317373c819ed
Justin Richards <justinrichards@Justins-MacBook-Air.local>
2015-11-02 11:32:49 -0600

    added most of the remaining interactivity to catch up with the last version

--- interrogate.js
+++ interrogate.js
@@ -1,27 +1,125 @@
 window.onload = function(){
 
+
+	window.nlp = nlp
+	window.jquery = $
+
+	// CHECK LOCAL STORAGE FOR CACHED DATA
+	if (!localStorage.artieLevel){
+		localStorage.artieLevel = 0
+	} 
+	if (!localStorage.juddLevel){
+		localStorage.juddLevel = 0
+	} 	// note: theScript will store the static, unchanging script for the game. localStorage will store the current STATE of the game. variable names can get similar.
+	localStorage.artieLevel = 0
+	localStorage.juddLevel = 0
 	// GLOBAL VARS
 	ENTER_CODE = 13
-	LEVELS = {
-		artie: localStorage.artieLevel ? localStorage.artieLevel : 0,
-		judd: localStorage.juddLevel ? localStorage.juddLevel : 0
-	}	
 
 	// UTILITY FUNCTIONS
+
+	Array.prototype.contains = function(el){
+		return this.indexOf(el) !== -1
+	}
+
+	Array.prototype.choice = function(){
+		return this[randInRange(0,this.length - 1)]
+	}
+
+	Node.prototype.clearChildren = function() {
+		while (this.firstChild){
+			this.removeChild(this.firstChild)
+		}
+	}
+
+	String.prototype.contains = function(subStr){
+		return this.indexOf(subStr) !== -1
+	}
+
+	var getResponse = function(inputStr,charName) {
+		var thisLevel = localStorage[charName + 'Level']
+			levelData = theScript[charName][thisLevel]
+		if (inputIsPersuasive(inputStr,charName)) {
+			localStorage[charName + 'Level'] = parseInt(localStorage[charName + 'Level'] + 1)
+			return levelData['advancementLine']
+		}
+		else {
+			return levelData['rotationLines'].choice()
+		}
+	}
+
+	var randInRange = function(low,high){
+		return Math.round(low + Math.random() * (high - low)) 
+	}
+
+	var replaceHint = function(node,newHint){
+		p = $('<p class="hintText">' + newHint + '</p>').css({opacity:0})
+		node.clearChildren()
+		node.appendChild(p[0])
+		p.animate({opacity:1},1000)
+	}
+
+	var toSingular = function(inputStr) {
+		var nouns = nlp.pos(inputStr).nouns()
+		nouns.forEach(function(noun){
+			var singular = nlp.noun(noun.text).singularize() // yes, that's the best way this api will allow this to happen
+			inputStr = inputStr.replace(noun.text,singular)
+		})
+		return inputStr
+	}
+
+	var toPresent = function(inputStr) {
+		var verbs = nlp.pos(inputStr).verbs()
+		verbs.forEach(function(verb){
+			var present = nlp.verb(verb.text).to_present() // yes, that's the best way this api will allow this to happen
+			inputStr = inputStr.replace(verb.text,present)
+		})
+		return inputStr
+	}
+
+	var inputIsPersuasive = function(inputStr,charName){
+		var thisLevel = localStorage[charName + 'Level']
+		window.script = theScript[charName]
+		console.log(thisLevel)
+		var	triggerTokens = theScript[charName][thisLevel]['triggerTokens']
+		normalStr = toSingular(toPresent(inputStr)),
+		tokenObjs = nlp.tokenize(normalStr)[0].tokens
+		inputTokens = tokenObjs.map(function(obj){return obj.normalised})
+		var found = true
+		triggerTokens.forEach(function(trigger){
+			if (!inputTokens.contains(trigger)) {
+				found = false
+			}
+		})
+		return found
+	}
 
 	// EVENT HANDLERS
 	var chatSubmissionHandler = function(e){
 		if (e.keyCode === ENTER_CODE) {
 			var charName = e.target.id,
-			response = theScript[charName][LEVELS[charName]].rotationLines[0],
+			ulSelector = 'ul#' + charName,
+			inputSelector = 'input#' + charName,
+			inputStr = $(inputSelector).val(),
+			response = getResponse(inputStr,charName),
 			responseEl = document.createElement('li')
 		responseEl.innerHTML = response
-		console.log(charName)
-		$('ul#' + charName).append(responseEl)
-		console.log(response)
+		$(ulSelector).append(responseEl)
+		$(ulSelector).scrollTop($(ulSelector)) // scroll to bottom of window, like in a chat interface
 		window.responseEl = responseEl
 		}
 	}
 
+	var showHint = function(e) {
+		var box = e.currentTarget
+			hintArtie = theScript['artie'][localStorage.artieLevel]['hint'],
+			hintJudd = theScript['judd'][localStorage.juddLevel]['hint'],
+			newHint = [hintArtie,hintJudd].choice(),
+			prompt = "Click here for a new hint."	
+		replaceHint(box,newHint)
+		setTimeout(function(){replaceHint(box,prompt)},10000) // hint fades away after 10s
+	}
+
 	$('input').keypress(chatSubmissionHandler)
+	$('#hintBox').click(showHint)
 }
--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 window.theScript = 
 	{
 		artie: {
-			0: {
-				hint: '',
-				triggerTokens: [],
+			'0': {
+				hint: 'The Tucker ‘48 is the biggest piece of evidence we have. No one else in three counties drives that car. But how can I prove that they were behind the wheel that night?',
+				triggerTokens: ['drives','tucker','48'],
 				rotationLines: [
-					"hi i'm wimpy artie"
+					"i'm wimpy",
+					"i'm nervous",
+					"i'm privileged"
 				],
-				advancementLine: ''
+				advancementLine: 'I don’t know much about cars, though the name does sound familiar.'
+			},
+			'1': {
+				hint: 'Have you ever driven it?',
+				triggerTokens: ['you','drives'],
+				rotationLines: [
+					"i'm wimpy",
+					"i'm nervous",
+					"i'm privileged"
+				],
+				advancementLine: 'I have never been allowed behind the wheel of a vehicle.'
 			}
 		},
 		judd: {
-			0: {
-				hint: '',
+			'0': {
+				hint: 'not an empty string',
 				triggerTokens: [],
 				rotationLines: [
 					"hi i'm cool judd"
